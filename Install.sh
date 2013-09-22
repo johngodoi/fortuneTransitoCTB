9e7f6a833493e7009f3f0c254bf5639258d66542
John Godoi <john.godoi@gmail.com>
2013-09-22 14:22:29 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,7 +2,8 @@
 
 if [ -e /usr/games/fortune ]; \
 then sudo mv dicasCarros /usr/share/games/fortunes/; \
-sudo strfile dicasCarros;
 sudo mv infracoesCTB /usr/share/games/fortunes/; \
+cd /usr/share/games/fortunes/; \
+sudo strfile dicasCarros; \
 sudo strfile infracoesCTB; \
 fi
